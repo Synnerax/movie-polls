ba4f4039622ecde5874d8f3b88189d04f642027a
jensengbg-elliot-berggren <elliot.berggren@hotmail.com>
2021-11-29 13:14:06 +0100

    Added backend folder

---
+++ backend/App.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const PORT = process.env.PORT || 5000;
+const helmet = require("helmet")
+//require("dotenv").config();
+
+app.use(cors());
+app.use(cookieParser());
+app.use(express.json());
+app.use(helmet());
+
+app.listen(PORT, () => {
+  console.log("server started on", PORT);
+});
+
